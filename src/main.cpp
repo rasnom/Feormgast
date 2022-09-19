eee988fea526c9db3e3696c3c283d8ce05fe5b28
Ransom Byers <ransom.byers@gmail.com>
2022-09-18 19:16:41 -0700

    Add WiFi Access Point and basic debug monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
-#include <Wifi.h>
+#include <WiFi.h>
+#include "secrets.h"
 
+#define LED 2
 
+const char *SSID = "Feormgast";
+const char *PASSWORD = AP_WIFI_PASSWORD;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // Create Wifi Access Point
+  Serial.print("Creating network ");
+  Serial.println(SSID);
+  Serial.println(PASSWORD);
+  WiFi.softAP(SSID, PASSWORD);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print(" at IP: ");
+  Serial.println(IP);
+
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+  delay(1000);
 }
