3ea64841bc8283476a35c3bb3108872dff5249d4
Ransom Byers <ransom.byers@gmail.com>
2022-09-26 21:04:35 -0700

    Add Update and connect to /update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Webserver.h>
-// #include <HTTPUpdateServer.h>
+#include <Update.h>
 #include "secrets.h"
 
 #define LED 2
@@ -12,8 +12,6 @@ const char *SSID = "Feormgast";
 const char *PASSWORD = AP_WIFI_PASSWORD;
 
 WebServer server(80);
-// HTTPUpdateServer httpUpdater;
-
 String header;
 
 bool motorOn = false;
@@ -23,30 +21,31 @@ unsigned long motorOnTime = 0;
 const long wifiTimeoutTime = 2000; // milliseconds
 const long motorDuration = 5000;
 
-const char* serverIndex = 
-  "<html>"
-    "<head>"
-      "<style>"
-        "html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}"
-        ".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;"
-        "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}"
-        ".button2 {background-color: #555555;}"
-      "</style>"
-    "</head>"
+const char *serverIndex = 
+  "<head>"
+    "<style>"
+      "html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}"
+      ".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;"
+      "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}"
+      ".button2 {background-color: #555555;}"
+    "</style>"
+  "</head>"
 
-    "<body>"
-      "<h1>Feormgast Web Server</h1>"
+  "<body>"
+    "<h1>Feormgast Web Server</h1>"
 
-      "<p><a href=\"/light/on\"><button class=\"button\">Turn ON</button></a></p>"
-      "<p><a href=\"/light/on\"><button class=\"button button2\">ALSO ON</button></a></p>"
+    "<p><a href=\"/light/on\"><button class=\"button\">Turn ON</button></a></p>"
+    "<p><a href=\"/light/on\"><button class=\"button button2\">ALSO ON</button></a></p>"
 
-      "<h2>Chicken Coop Door</h2>"
-      "<p><a href=\"/door/open\"><button class=\"button\">OPEN</button></a></p>"
-      "<p><a href=\"/door/close\"><button class=\"button\">CLOSE</button></a></p>"
-
-    "</body>"
-  "</html>";
+    "<h2>Chicken Coop Door</h2>"
+    "<p><a href=\"/door/open\"><button class=\"button\">OPEN</button></a></p>"
+    "<p><a href=\"/door/close\"><button class=\"button\">CLOSE</button></a></p>"
+  "</body>";
   
+const char *updateForm = 
+  "<form method='POST' action='/update' enctype='multipart/form-data'>"
+    "<input type='file' name='update'><input type='submit' value='Update'>"
+  "</form>";
 
 void setupRoutes() {
   server.on("/", HTTP_GET, []() {
@@ -79,6 +78,37 @@ void setupRoutes() {
     motorOnTime = millis();
     digitalWrite(CLOSEPIN, HIGH);
   });
+  server.on("/update", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", updateForm);
+  });
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.setDebugOutput(true);
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin()) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) {
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+      Serial.setDebugOutput(false);
+    } else {
+      Serial.printf("Update Failed: status is %d\n", upload.status);
+    }
+  });
   server.onNotFound( []() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/html", serverIndex);
@@ -98,7 +128,6 @@ void setup() {
   Serial.print(" at IP: ");
   Serial.println(IP);
 
-  // httpUpdater.setup(&server);
   setupRoutes();
   server.begin();
 
@@ -111,15 +140,12 @@ void setup() {
 }
 
 void loop() {
-  // WiFiClient client = server.available();
-
   if (motorOn && millis() - motorOnTime >= motorDuration) {
     digitalWrite(OPENPIN, LOW);
     digitalWrite(CLOSEPIN, LOW);
     digitalWrite(LED, LOW);
     motorOn = false;
   }
-
   server.handleClient();
   delay(2);
 }
