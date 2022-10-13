306407e52389e34ef8c2eb57fdd2ac62b2961d6d
Ransom Byers <ransom.byers@gmail.com>
2022-10-12 23:57:50 -0700

    Breakout updateFirmware()

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,30 @@ void clockSync() {
   server.send(200, "text/html", serverIndex());
 }
 
+void updateFirmware() {
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    Serial.setDebugOutput(true);
+    Serial.printf("Update: %s\n", upload.filename.c_str());
+    if (!Update.begin()) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) {
+      Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+    } else {
+      Update.printError(Serial);
+    }
+    Serial.setDebugOutput(false);
+  } else {
+    Serial.printf("Update Failed: status is %d\n", upload.status);
+  }
+}
+
 void setupRoutes() {
   server.on("/", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
@@ -102,29 +126,7 @@ void setupRoutes() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
     ESP.restart();
-  }, []() {
-    HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
-      Serial.setDebugOutput(true);
-      Serial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin()) {
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) {
-        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
-        Update.printError(Serial);
-      }
-      Serial.setDebugOutput(false);
-    } else {
-      Serial.printf("Update Failed: status is %d\n", upload.status);
-    }
-  });
+  }, updateFirmware);
   server.on("/script.js", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/javascript", javaScript());
