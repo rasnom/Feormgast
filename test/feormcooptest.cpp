95f8bc489c5a5de13c197e5e38391cf2bde701e4
Ransom Byers <ransom.byers@gmail.com>
2022-10-22 23:42:44 -0700

    Add Google Test for unit testing & feormcooptest.h

---
+++ test/feormcooptest.cpp
@@ -0,0 +1,32 @@
+#include "feormcoop.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+    TEST(FeormCoop, TestingTest) {
+        EXPECT_EQ(1, 1);
+        EXPECT_NE(1, 0);
+        EXPECT_GT(1, 0);
+    }
+
+    TEST(FeormCoop, AnotherTest) {
+        EXPECT_EQ(1, 0);
+        EXPECT_LT(1, 0);
+        EXPECT_EQ(2, 2);
+    }
+
+}
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial) { delay(10); };
+
+    ::testing::InitGoogleTest();    
+}
+
+void loop() {
+    if (RUN_ALL_TESTS());
+
+    delay(1000);
+}
+
